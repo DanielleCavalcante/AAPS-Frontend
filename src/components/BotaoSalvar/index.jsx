0618eef29b62c0c0dd4a92a20615dc90e5666111
Adriana Akagui <adriana_akagui@yahoo.com.br>
2024-11-13 01:59:58 -0300

    botao

--- src/components/BotaoSalvar/index.jsx
+++ src/components/BotaoSalvar/index.jsx
@@ -6,7 +6,10 @@ const botaoSalvar = ({showModal, openModal, closeModal}) => {
   return (
     
     <div>
-      <button className="btn-Salvar" onClick={openModal}>Salvar</button>
+      <button className="btn-Salvar" onClick={openModal}>
+        <i class="fa fa-floppy" aria-hidden="true"></i>
+        <span>Salvar</span>
+      </button>
 
       <Modal show={showModal} onClose={closeModal}>
         <img src="/src/assets/emoji-smile.png" alt="Ícone de sucesso" className="icon" />
