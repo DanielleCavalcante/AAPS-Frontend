d6d26498873694c9912754bb173df728f2440461
Adriana Akagui <adriana_akagui@yahoo.com.br>
2024-11-14 23:07:14 -0300

    doador

--- src/pages/AlterarDoador/index.jsx
+++ src/pages/AlterarDoador/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './cadastrodoador.css';
+import './alterarDoador.css';
 
 const CadastroDoador = () => {
     const [telefones, setTelefones] = useState(['']);
