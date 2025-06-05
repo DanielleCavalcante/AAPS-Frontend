404ec26a7d1409db469d9d308c1f54afb0ec0314
Adriana Akagui <adriana_akagui@yahoo.com.br>
2025-06-04 21:20:23 -0300

    botão anular em adoção e alterado o nome de exibição de Evento para Procedimento

--- src/pages/Doadores/CadastrarDoador/CadastrarDoador.jsx
+++ src/pages/Doadores/CadastrarDoador/CadastrarDoador.jsx
@@ -5,7 +5,7 @@ import { useBuscarCep } from '../../../hooks/useBuscarCep';
 import { useNavigate } from 'react-router-dom';
 import InputMask from 'react-input-mask';
 import { validarCPF } from '../../../utils/ValidaCPF';
-import { validarRG } from '../../../utils/validaRG';
+import { validarRG } from '../../../utils/ValidaRG';
 import { validarNome } from '../../../utils/ValidaNome';
 
 import BotaoSalvar from "/src/components/BotaoSalvar/BotaoSalvar.jsx";
