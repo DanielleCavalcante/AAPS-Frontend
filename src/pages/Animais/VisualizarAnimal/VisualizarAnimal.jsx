0a0c50a44992f81f15e98dd68819d44b474ef494
DanielleCavalcante <daniellesoucavalcante1324@gmail.com>
2025-06-06 21:23:14 -0300

    finalizado integracao tela visualizar/atualizar adocao

--- src/pages/Animais/VisualizarAnimal/VisualizarAnimal.jsx
+++ src/pages/Animais/VisualizarAnimal/VisualizarAnimal.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 
 import { useAnimais } from '../../../hooks/useAnimais';
 import { useDoadores } from '../../../hooks/useDoadores';
